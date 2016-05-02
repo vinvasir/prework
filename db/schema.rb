fcfeec4bbcf98450aed1981ddda79dbabb44d95a
Prateek Vasireddy <prat.vasireddy@gmail.com>
2016-05-02 18:24:30 +0000

    Added color and age fields to cats

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160502181151) do
+ActiveRecord::Schema.define(version: 20160502182151) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 20160502181151) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "color"
+    t.integer  "age"
   end
 
 end
